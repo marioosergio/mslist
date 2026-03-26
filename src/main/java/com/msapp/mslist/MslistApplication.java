70bac255d86e691d58ec4aa622542f263f46d5ff
Mario Sergio <msergioc@gmail.com>
2026-03-26 02:10:21 -0300

    Project created

---
+++ src/main/java/com/msapp/mslist/MslistApplication.java
@@ -0,0 +1,13 @@
+package com.msapp.mslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslistApplication.class, args);
+	}
+
+}
