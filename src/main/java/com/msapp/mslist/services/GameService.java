0e1432075fc48c32d6cdebbec8e6d0b87a05fb7b
Mario Sergio <msergioc@gmail.com>
2026-04-01 23:02:07 -0300

    SQL, projection, find games by list

--- src/main/java/com/msapp/mslist/services/GameService.java
+++ src/main/java/com/msapp/mslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.msapp.mslist.services;
 import com.msapp.mslist.dto.GameDTO;
 import com.msapp.mslist.dto.GameMinDTO;
 import com.msapp.mslist.entities.Game;
+import com.msapp.mslist.projections.GameMinProjection;
 import com.msapp.mslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
